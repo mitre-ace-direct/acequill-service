297dd302b3ab3b188fd737c549237964e0d78b1b
Mark Riehl <mriehl@mitre.org>
2018-10-09 10:36:11 -0400

    Changed map if()

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ function handle_manager_event(evt) {
 
             console.log("Get return: '" + bridgeIdMap.get(bridgeId) + "'");
 
-            if (bridgeIdMap.get(bridgeId) === "undefined") {
+            if (bridgeIdMap.get(bridgeId) === undefined) {
               console.log("Received first leg of the call, creating map");
               console.log(bridgeId + " => " + channel);
 
