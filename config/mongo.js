7afb286a2a5956aa6ed6b592cd9cda833d0da026
Woodman <MWOODMAN@MITRE.ORG>
2019-05-08 15:49:27 -0400

    adding pm2 process file

---
+++ config/mongo.js
@@ -0,0 +1,3 @@
+module.exports = {
+    uri: process.env.MONGODB_URI || "uri string", // Set string if not using ENV
+}
--- config/watson.js
+++ config/watson.js
@@ -1,6 +1,6 @@
 module.exports = {
-    username: process.env.WATSON_USERNAME,
-    password: process.env.WATSON_PASSWORD,
+    username: process.env.WATSON_USERNAME || "username-string", // Set string if not using ENV
+    password: process.env.WATSON_PASSWORD || "password-string", // Set string if not using ENV
     contentType: "audio/wav; rate=16000",
     smartFormatting: true
 }
