9eff71ed0902b5e73fe261ee30df91ef77a9c2e7
Mark Riehl <mriehl@mitre.org>
2019-09-06 10:38:24 -0400

    Updated check on data.results, added missing semicolons.

--- transcription/watson.js
+++ transcription/watson.js
@@ -26,14 +26,14 @@ function Watson(file, configs) {
     this.proxy_port = configs.proxy_port;
     this.contentType = "audio/wav; rate=16000";
     this.smart_formatting = true;
-};
+}
 
 Watson.prototype.start = function (callback) {
 
     var speechParams = {
         iam_apikey: this.iam_apikey,
         url: this.url
-    }
+    };
 
     if (this.proxy) {
         var tunnel = require('tunnel');
@@ -67,12 +67,13 @@ Watson.prototype.start = function (callback) {
                 'final': data.results[0].final,
                 'timestamp': new Date()
             };
+
+            console.log('results:' + results);
+            callback(results);
         }
 
-        console.log('results:' + results);
-        callback(results);
     }).on('open', function () {
-        console.log("Websocket to watson is open. Resume GrowingFile.")
+        console.log("Websocket to watson is open. Resume GrowingFile.");
         gf.resume();
     }).on('error', function (err) {
         console.log(err.toString());
@@ -95,7 +96,7 @@ Watson.prototype.start = function (callback) {
     }).on('end', () => {
         console.log('FILE HAS ENDED');
         recognizeStream.finish();
-    })
+    });
 
 };
 
