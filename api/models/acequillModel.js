3f6538cf8267e704a53f9dae43b482b53b72bf4d
Mike Woodman (mwoodman@mitre.org) <mwoodman@ace40asterisk.task3acrdemo.com>
2020-07-16 19:11:25 +0000

    tranlation api

--- api/models/acequillModel.js
+++ api/models/acequillModel.js
@@ -1,6 +1,12 @@
+const Watson = require('./../../translation/watson'),
+    configs = require('./../../config/watson');
+
 exports.translate = function(text, from, to, cb) {
-  let s = text.split("");
-  let r = s.reverse();
-  let translation = r.join("")
-  cb(null, {translation: translation})
+  //let s = text.split("");
+  //let r = s.reverse();
+  //let translation = r.join("")
+  let engine = new Watson(configs);
+  engine.translate(text, from, to, function(translation){
+      cb(null, {translation: translation})
+  });
 }
--- config/watson.js
+++ config/watson.js
@@ -1,6 +1,8 @@
  module.exports = {
-    iam_apikey: process.env.WATSON_IAM_APIKEY || "<API KEY HERE>", // Set string if not using ENV
-    url: process.env.WATSON_URL ||  "<WATSON URL HERE>", // Set string if not using ENV
-    proxy: '<PROXY IP HERE>',
-    proxy_port: '<PROXY PORT HERE>'
+    iam_apikey: "xTydnpW9xtIWFeXU1x0QLa8k3xv1KyC-NEt521sueK6a",
+    url: "https://stream.watsonplatform.net/speech-to-text/api",
+    translation_apikey : "0n1AU1EXG5if-MZBE0sQF2hDJNS8fwRN4H29zSbyezw5",
+    translation_url: "https://gateway.watsonplatform.net/language-translator/api",
+    proxy: '10.202.1.215',
+    proxy_port: '3128'
 }
