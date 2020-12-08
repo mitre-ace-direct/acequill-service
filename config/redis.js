552967b00622d1503fd24f9c1be617e296b6773e
EC2 Default User <ec2-user@qa40asterisk.task3acrdemo.com>
2020-12-08 18:21:59 +0000

    added redis to modules

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: process.env.REDIS_HOST || "172.21.1.107", // Set string if not using ENV
+    host: process.env.REDIS_HOST || "172.21.1.83", // Set string if not using ENV
     port: process.env.REDIS_PORT ||  "6379", // Set string if not using ENV
     auth: process.env.REDIS_AUTH ||  "NotUsedInDevEnvironment", // Set string if not using ENV
-    hashname: process.env.REDIS_HASHNAME ||  "jkorb_extensionToLanguage", // Set string if not using ENV
+    hashname: process.env.REDIS_HASHNAME ||  "ec2-user_extensionToLanguage", // Set string if not using ENV
 }
