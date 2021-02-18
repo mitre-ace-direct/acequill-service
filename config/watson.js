d198336e1280c5bad4a1a521cd438e9205f41146
Mike Woodman <mwoodman@mitre.org>
2021-02-18 12:36:31 -0500

    Removed credentials

--- config/watson.js
+++ config/watson.js
@@ -1,8 +1,8 @@
 module.exports = {
-    iam_apikey: "xTydnpW9xtIWFeXU1x0QLa8k3xv1KyC-NEt521sueK6a",
+    iam_apikey: "<speech to text api key>",
     url: "https://stream.watsonplatform.net/speech-to-text/api",
-    translation_apikey : "0n1AU1EXG5if-MZBE0sQF2hDJNS8fwRN4H29zSbyezw5",
+    translation_apikey : "<translation-api-key>",
     translation_url: "https://gateway.watsonplatform.net/language-translator/api",
-    proxy: '10.202.1.215',
-    proxy_port: '3128'
+    proxy: "<proxy ip or hostname>",
+    proxy_port: "<proxy_port>"
 }
