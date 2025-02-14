66791cdd4dbd0e92431637825a32bd76c87fd877
Edwin Torres <CoachEd@gmail.com>
2025-02-14 14:16:38 -0500

    initial

---
+++ README.md
@@ -0,0 +1,76 @@
+# ACE Quill Service
+
+Accessible Communications for Everyone (ACE) Direct is a Direct Video Calling
+(DVC) platform that enables direct calling from deaf and hard-of-hearing
+individuals to an American Sign Language (ASL)-trained agent in an
+organization's call center. The agent answers the call using a web browser
+which provides a real-time video connection to a consumer.
+
+One of the features of the ACE Direct is the ability to display captions on
+both the agent and consumer portals. ACE Direct uses the IBM Watson automated
+speech recognition (ASR) engine. Audio from the video call is captured and sent
+to the IBM Watson ASR and captioned text is returned for display on both the
+agent and consumer portals.
+
+### Getting Started
+Probably the *best* way to install the entire ACE Direct system is to start with
+the acedirect-public repo. Follow the documentation there for a clean install.
+The CHECKLISTS.md file provides an overview of the complete installation and
+configuration process.
+
+The ACE Quill service provides caption support in ACE Direct. Because the ACE
+Quill service resides on a different server, it isn't part of the automated
+installation script.
+
+Note, captions are optional in ACE Direct and will require an IBM Account and
+credit card for billing. Pricing information for the Watson captioning service
+can be found [here](https://www.ibm.com/cloud/watson-speech-to-text/pricing).
+
+### Install the ACE Quill Captioning Service
+1. Clone this repository onto the same server that is running Asterisk
+1. Clone the dat repo in the same folder and follow the configuration
+instructions.
+1. Download and install [Node.js](https://nodejs.org/en/) v16 (Active LTS Release)
+1. In an elevated command prompt, run `npm install -g pm2`
+
+### Google Configuration
+1. The ACE Quill service uses the Google speech to text engine to support
+captions and requires an [Google](https://cloud.google.com/speech-to-text) account and
+credit card to support billing
+1. Create a Google account and create a speech to text service
+1. Download a private key as a JSON from the `Service Accounts` menu
+1. Copy the JSON file to config/google.json
+
+### IBM Watson Configuration (optional)
+*ACE Quill can use IBM Watson inplace of Google for Speech to Text and Translation.
+If you wish to use IBM Watson instead of Google you will need to change the following files:*         
+
+`service.js`:  uncomment line 3 and comment out line 4.  
+`api/models/acequillModel.js`: uncomment line 1 and comment out line 2.
+
+#### IBM Watson Service Account 
+
+1. The ACE Quill service uses the IBM Watson speech to text engine to support
+captions and requires an [IBM Cloud](https://www.ibm.com/cloud) account and
+credit card to support billing
+1. Create an IBM Watson account and create a speech to text resource
+1. Download the credentials file which should have the following format:
+```
+SPEECH_TO_TEXT_IAM_APIKEY=<API KEY HERE>
+SPEECH_TO_TEXT_URL=<WATSON URL HERE>
+```
+1. Copy the API key and URL into config/watson.js
+
+### Starting the Service
+1. To start the ACE Quill node server with pm2, run `pm2 start process.json`
+1. To verify the service is running, type `pm2 status` and you should see output similar to this:
+
+```
+
+┌─────┬─────────────────────┬─────────────┬─────────┬─────────┬──────────┬────────┬──────┬───────────┬──────────┬──────────┬──────────┬──────────┐
+│ id  │ name                │ namespace   │ version │ mode    │ pid      │ uptime │ ↺    │ status    │ cpu      │ mem      │ user     │ watching │
+├─────┼─────────────────────┼─────────────┼─────────┼─────────┼──────────┼────────┼──────┼───────────┼──────────┼──────────┼──────────┼──────────┤
+│ 1   │ ACEQuill-API        │ default     │ 1.0.0   │ fork    │ 4755     │ 0s     │ 0    │ online    │ 0%       │ 15.0mb   │ centos │ disabled │
+│ 0   │ ACEQuill-Service    │ default     │ 1.0.0   │ fork    │ 4753     │ 0s     │ 0    │ online    │ 0%       │ 16.0mb   │ centos │ disabled │
+└─────┴─────────────────────┴─────────────┴─────────┴─────────┴──────────┴────────┴──────┴───────────┴──────────┴──────────┴──────────┴──────────┘
+```
---
+++ service.js
@@ -0,0 +1,300 @@
+const asteriskManager = require('asterisk-manager');
+const asteriskConfigs = require('./config/asterisk');
+//const STTEngine = require('./transcription/watson');
+const STTEngine = require('./transcription/google');
+const RedisManager = require('./utils/redisManager');
+const cleaner = require('./utils/cleaner');
+var wavFilePath = process.cwd() + '/recordings/';
+console.log(wavFilePath)
+var bridgeIdMap = new Map();
+var channelIdSet = new Set();
+var ami = null;
+var nconf = require('nconf');
+var fs = require('fs');
+
+/*
+** The presence of a populated cleartext field in config.json means that the file is in clear text
+** remove the field or set it to "" if the file is encoded
+*/
+var clearText = false;
+if (typeof (nconf.get('common:cleartext')) !== "undefined" && nconf.get('common:cleartext') !== "") {
+    console.log('clearText field is in config.json. assuming file is in clear text');
+    clearText = true;
+}
+
+//Comment out if using IBM Watson
+process.env.GOOGLE_APPLICATION_CREDENTIALS = process.cwd() + "/config/google.json";
+
+/**
+ * Creates an AMI connection to Asterisk.
+ */
+function init_ami() {
+    if (ami === null) {
+        try {
+            ami = new asteriskManager(
+                asteriskConfigs.port,
+                asteriskConfigs.host,
+                asteriskConfigs.user,
+                asteriskConfigs.password,
+                true);
+
+            ami.keepConnected();
+            // Define event handlers here
+            ami.on('managerevent', handle_manager_event);
+            console.log('Connected to Asterisk');
+
+        } catch (exp) {
+            console.log('Init AMI error' + JSON.stringify(exp));
+        }
+    }
+}
+
+// Create Redis Client
+const rClient = new RedisManager();
+// Initialize the Asterisk AMI connection
+init_ami();
+
+setInterval(function(){
+	cleaner(wavFilePath);
+},5000);
+
+
+/**
+ * Event handler for AMI events coming from Asterisk.
+ * @param {object} evt - AMI event from Asterisk.
+ */
+function handle_manager_event(evt) {
+
+    switch (evt.event) {
+
+        case ('BridgeEnter'):
+            /*
+             * BridgeEnter handler logic:
+             * 1. For each call, we will receive two BridgeEnter events, one for each leg of the call
+             * 2. When we receive the 2nd bridge event, verify that it has the same bridge ID
+             *   (showing that it is the other leg of the same call)
+             * 3. Retrieve channel IDs for each side of the call (required to record the call)
+             * 4. Call the startTranscription() function for each leg of the call
+             */
+
+            console.log();
+            console.log('****** BridgeEnter ******');
+            console.log(JSON.stringify(evt, null, 4));
+
+            // Extract the Bridge ID and the channel from the event
+            var bridgeId = evt.bridgeuniqueid; // Looks like 'd1084052-f50a-4c5d-b459-354e832a9ff5'
+            var channel = evt.channel; // Looks like 'PJSIP/30001-0000001f'
+
+            console.log("bridgeId: " + bridgeId);
+            console.log("channel: " + channel);
+
+            console.log("bridgeIdMap.size: " + bridgeIdMap.size);
+            console.log("Get return: '" + bridgeIdMap.get(bridgeId) + "'");
+
+            if (bridgeIdMap.get(bridgeId) === undefined) {
+
+                // We haven't seen this bridgeId before, so, store the bridgeId and channel for the first leg of the call
+                bridgeIdMap.set(bridgeId, channel);
+
+                console.log("Received first leg of the call, creating map");
+                console.log(bridgeId + " => " + channel);
+            } else {
+
+                // This bridgeId is in the map, so, this is the second leg of the call
+                console.log("Received second leg of the call");
+                console.log("bridgeId: " + bridgeId + " => channel: " + channel);
+                console.log();
+
+                // Get the agent channel from the map
+                var agentChannel = "NOT FOUND";
+                if (bridgeIdMap.has(bridgeId)) {
+                    agentChannel = bridgeIdMap.get(bridgeId);
+                }
+
+                // The consumer channel just arrived in the second BridgeEnter event
+                var consumerChannel = channel;
+
+                console.log("agentChannel: " + bridgeIdMap.get(bridgeId));
+                console.log("consumerChannel: " + consumerChannel);
+
+                console.log("Clearing map");
+                console.log("bridgeIdMap.size - before: " + bridgeIdMap.size);
+
+                // We are finished with this bridgeId, so, remove it from the map
+                bridgeIdMap.delete(bridgeId);
+
+                console.log("bridgeIdMap.size - after: " + bridgeIdMap.size);
+
+                var wavFilename = wavFilePath +  bridgeId;
+
+                console.log("Adding " + agentChannel + " to set");
+                channelIdSet.add(agentChannel);
+
+                // Start recording here
+                console.log("Recording file: " + wavFilePath + wavFilename);
+
+                var mixMonitorCommand = {
+                    Action: "MixMonitor",
+                    Channel: evt.channel,
+                    File: wavFilename + "-mix.wav16",
+                    options: "r(" + wavFilename + "-callee-out.wav16) t(" + wavFilename + "-caller-out.wav16)"
+                };
+
+                sendAmiAction(mixMonitorCommand);
+
+
+
+                /*
+                 * Build the filenames to pass out to startTransciption, Asterisk appends the
+                 * -in.wav16 and -out.wav16 extensions to the files is creates
+                 */
+                var inFile =  wavFilename + "-caller-out.wav16";
+                var outFile = wavFilename + "-callee-out.wav16";
+
+                console.log();
+                console.log("inFile: " + inFile);
+                console.log("outFile: " + outFile);
+                console.log("uniqueid:" + evt.uniqueid);
+                console.log("consumerChannel: " + consumerChannel);
+                console.log("agentChannel: " + agentChannel);
+
+                // Start the transcription for each channel
+                // Test if extension is webrtc (30000 or 90000)
+                const webrtcExt = new RegExp("PJSIP\/(3|9)");
+                if (webrtcExt.test(consumerChannel)) {
+                    rClient.getLanguageByExtension(agentChannel.substring(6, 11), function (langCd) {
+                        startTranscription(inFile, consumerChannel, evt.uniqueid, langCd);
+                    });
+                }
+                if (webrtcExt.test(agentChannel)) {
+                    rClient.getLanguageByExtension(consumerChannel.substring(6, 11), function (langCd) {
+                        startTranscription(outFile, agentChannel, evt.uniqueid, langCd);
+                    });
+                }
+            }
+            break;
+
+        case ('Hangup'):
+            console.log();
+            console.log('****** Hangup ******');
+            console.log(JSON.stringify(evt, null, 4));
+
+            /*
+             * If this set has the channel we stored earlier, use this to send an AMI action
+             * to Asterisk and stop recording. Note, we only need to call stop once on
+             * this channel (corresponds to the Monitor action above).
+             * */
+            if (channelIdSet.has(evt.channel)) {
+
+                console.log("Found a match in the set for " + evt.channel);
+
+                sendAmiAction({
+                    "Action": "StopMonitor",
+                    "Channel": evt.channel
+                });
+
+                // Remove this channel from the set, we're all finished with it
+                channelIdSet.delete(evt.channel);
+            }
+            break;
+    }
+}
+
+/**
+ * Contacts IBM Watson and starts captioning this channel.
+ * @param {string} wavFile - Name of the WAV file being populated.
+ * @param {string} channel - Asterisk channel corresponding to this leg of the call.
+ */
+function startTranscription(wavFile, channel, callid, langCd) {
+
+    console.log("Entering startTranscription - wavFile: " + wavFile);
+
+    try {
+        var sttEngineMsgTime = 0;
+        var sttEngine = new STTEngine(wavFile, langCd);
+
+        sttEngine.start(function (data) {
+
+            if (sttEngineMsgTime === 0) {
+                let d = new Date();
+                sttEngineMsgTime = d.getTime();
+            }
+            data.langCd = langCd;
+            data.msgid = sttEngineMsgTime;
+
+
+            //Remove comment for Debugging only, privacy violation to write transcripts to log file
+            //console.log("data.msgid: " + data.msgid);
+            //console.log("data.transcript: " + data.transcript);
+
+            if (channel) {
+                sendAmiAction({
+                    "Action": "SendText",
+                    "ActionID": data.msgid,
+                    "Channel": channel,
+                    "Message": JSON.stringify(data)
+                });
+
+                data.channel = channel;
+                data.callid = callid;
+
+            }
+
+            if (data.final) {
+                // reset message time
+                sttEngineMsgTime = 0;
+            }
+        });
+
+    } catch (err) {
+        console.log('Error Initializing Watson');
+        console.log(err);
+    }
+}
+
+
+/**
+ * Sends an AMI action to Asterisk.
+ * @param {JSON} obj - Contains AMI action (in JSON format) to be executed.
+ */
+function sendAmiAction(obj) {
+
+    ami.action(obj, function (err, res) {
+        if (err) {
+            console.log('AMI Action error ' + JSON.stringify(err, null, 4));
+        }
+    });
+}
+
+/**
+ * Function to verify the config parameter name and decode it from Base64 (if necessary).
+ * @param {type} param_name - The config parameter we are trying to retrieve.
+ * @returns {unresolved} Decoded readable string.
+ */
+function getConfigVal(param_name) {
+    var val = nconf.get(param_name);
+    var decodedString = null;
+
+    if (typeof val !== 'undefined' && val !== null) {
+        //found value for param_name
+
+
+        if (clearText) {
+
+            decodedString = val;
+        } else {
+            decodedString = new Buffer(val, 'base64');
+        }
+    } else {
+        //did not find value for param_name
+        /*
+        logger.error('');
+        logger.error('*******************************************************');
+        logger.error('ERROR!!! Config parameter is missing: ' + param_name);
+        logger.error('*******************************************************');
+        logger.error('');
+        */
+        decodedString = "";
+    }
+    return (decodedString.toString());
+}
